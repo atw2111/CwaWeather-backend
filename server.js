0115e0963612bb0a6508bf2e43e6a0b1fd0703f0
michellecylin <michellecylin@gmail.com>
2025-12-07 13:09:45 +0800

    變更成臺東縣

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const getKaohsiungWeather = async (req, res) => {
       {
         params: {
           Authorization: CWA_API_KEY,
-          locationName: "嘉義縣",
+          locationName: "臺東縣",
         },
       }
     );
